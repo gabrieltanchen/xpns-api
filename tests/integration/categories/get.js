84e4644289aab0e3a90b3720f318db2a6c14d3ef
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-07-25 07:42:16 -0400

    GET test

--- tests/integration/categories/get.js
+++ tests/integration/categories/get.js
@@ -50,6 +50,34 @@ describe.only('Integration - GET /categories', function() {
   let user2Token;
   let user2Uuid;
 
+  // 24 Addison
+  // 22 Alexis
+  // 20 Angel
+  // 15 Arlie
+  // 7 Austyn
+  // 8 Bruno
+  // 26 Caelan
+  // 18 Cassidy
+  // 19 Charlie
+  // 3 Garret
+  // 11 Gerry
+  // 21 Justice
+  // 27 Laurie
+  // 16 Leith
+  // 14 Lindsey
+  // 17 Meade
+  // 9 Morven
+  // 13 Murphy
+  // 6 Myles
+  // 25 Payton
+  // 1 Roland
+  // 23 Selby
+  // 5 Shayne
+  // 12 Taylor
+  // 10 Thomas
+  // 4 Titus
+  // 2 Winfield
+
   before('get server', async function() {
     this.timeout(30000);
     const app = await testHelper.getApp();
@@ -116,7 +144,7 @@ describe.only('Integration - GET /categories', function() {
 
   before('create category 2', async function() {
     const apiCall = await models.Audit.ApiCall.create({
-      user_uuid: user2Uuid,
+      user_uuid: user1Uuid,
     });
     category2Uuid = await controllers.CategoryCtrl.createCategory({
       auditApiCallUuid: apiCall.get('uuid'),
@@ -404,7 +432,194 @@ describe.only('Integration - GET /categories', function() {
     });
   });
 
-  it('should return 200 and 25 categories as user 1 with no limit or page specified');
+  it('should return 200 and 25 categories as user 1 with no limit or page specified', async function() {
+    const res = await chai.request(server)
+      .get('/categories')
+      .set('Content-Type', 'application/vnd.api+json')
+      .set('Authorization', `Bearer ${user1Token}`);
+    expect(res).to.have.status(200);
+    assert.isOk(res.body.data);
+    assert.strictEqual(res.body.data.length, 25);
+
+    // Category 24
+    assert.isOk(res.body.data[0].attributes);
+    assert.isOk(res.body.data[0].attributes['created-at']);
+    assert.strictEqual(res.body.data[0].attributes.name, sampleData.categories.category24.name);
+    assert.strictEqual(res.body.data[0].id, category24Uuid);
+    assert.strictEqual(res.body.data[0].type, 'categories');
+
+    // Category 22
+    assert.isOk(res.body.data[1].attributes);
+    assert.isOk(res.body.data[1].attributes['created-at']);
+    assert.strictEqual(res.body.data[1].attributes.name, sampleData.categories.category22.name);
+    assert.strictEqual(res.body.data[1].id, category22Uuid);
+    assert.strictEqual(res.body.data[1].type, 'categories');
+
+    // Category 20
+    assert.isOk(res.body.data[2].attributes);
+    assert.isOk(res.body.data[2].attributes['created-at']);
+    assert.strictEqual(res.body.data[2].attributes.name, sampleData.categories.category20.name);
+    assert.strictEqual(res.body.data[2].id, category20Uuid);
+    assert.strictEqual(res.body.data[2].type, 'categories');
+
+    // Category 15
+    assert.isOk(res.body.data[3].attributes);
+    assert.isOk(res.body.data[3].attributes['created-at']);
+    assert.strictEqual(res.body.data[3].attributes.name, sampleData.categories.category15.name);
+    assert.strictEqual(res.body.data[3].id, category15Uuid);
+    assert.strictEqual(res.body.data[3].type, 'categories');
+
+    // Category 7
+    assert.isOk(res.body.data[4].attributes);
+    assert.isOk(res.body.data[4].attributes['created-at']);
+    assert.strictEqual(res.body.data[4].attributes.name, sampleData.categories.category7.name);
+    assert.strictEqual(res.body.data[4].id, category7Uuid);
+    assert.strictEqual(res.body.data[4].type, 'categories');
+
+    // Category 8
+    assert.isOk(res.body.data[5].attributes);
+    assert.isOk(res.body.data[5].attributes['created-at']);
+    assert.strictEqual(res.body.data[5].attributes.name, sampleData.categories.category8.name);
+    assert.strictEqual(res.body.data[5].id, category8Uuid);
+    assert.strictEqual(res.body.data[5].type, 'categories');
+
+    // Category 26
+    assert.isOk(res.body.data[6].attributes);
+    assert.isOk(res.body.data[6].attributes['created-at']);
+    assert.strictEqual(res.body.data[6].attributes.name, sampleData.categories.category26.name);
+    assert.strictEqual(res.body.data[6].id, category26Uuid);
+    assert.strictEqual(res.body.data[6].type, 'categories');
+
+    // Category 18
+    assert.isOk(res.body.data[7].attributes);
+    assert.isOk(res.body.data[7].attributes['created-at']);
+    assert.strictEqual(res.body.data[7].attributes.name, sampleData.categories.category18.name);
+    assert.strictEqual(res.body.data[7].id, category18Uuid);
+    assert.strictEqual(res.body.data[7].type, 'categories');
+
+    // Category 19
+    assert.isOk(res.body.data[8].attributes);
+    assert.isOk(res.body.data[8].attributes['created-at']);
+    assert.strictEqual(res.body.data[8].attributes.name, sampleData.categories.category19.name);
+    assert.strictEqual(res.body.data[8].id, category19Uuid);
+    assert.strictEqual(res.body.data[8].type, 'categories');
+
+    // Category 3
+    assert.isOk(res.body.data[9].attributes);
+    assert.isOk(res.body.data[9].attributes['created-at']);
+    assert.strictEqual(res.body.data[9].attributes.name, sampleData.categories.category3.name);
+    assert.strictEqual(res.body.data[9].id, category3Uuid);
+    assert.strictEqual(res.body.data[9].type, 'categories');
+
+    // Category 11
+    assert.isOk(res.body.data[10].attributes);
+    assert.isOk(res.body.data[10].attributes['created-at']);
+    assert.strictEqual(res.body.data[10].attributes.name, sampleData.categories.category11.name);
+    assert.strictEqual(res.body.data[10].id, category11Uuid);
+    assert.strictEqual(res.body.data[10].type, 'categories');
+
+    // Category 21
+    assert.isOk(res.body.data[11].attributes);
+    assert.isOk(res.body.data[11].attributes['created-at']);
+    assert.strictEqual(res.body.data[11].attributes.name, sampleData.categories.category21.name);
+    assert.strictEqual(res.body.data[11].id, category21Uuid);
+    assert.strictEqual(res.body.data[11].type, 'categories');
+
+    // Category 27
+    assert.isOk(res.body.data[12].attributes);
+    assert.isOk(res.body.data[12].attributes['created-at']);
+    assert.strictEqual(res.body.data[12].attributes.name, sampleData.categories.category27.name);
+    assert.strictEqual(res.body.data[12].id, category27Uuid);
+    assert.strictEqual(res.body.data[12].type, 'categories');
+
+    // Category 16
+    assert.isOk(res.body.data[13].attributes);
+    assert.isOk(res.body.data[13].attributes['created-at']);
+    assert.strictEqual(res.body.data[13].attributes.name, sampleData.categories.category16.name);
+    assert.strictEqual(res.body.data[13].id, category16Uuid);
+    assert.strictEqual(res.body.data[13].type, 'categories');
+
+    // Category 14
+    assert.isOk(res.body.data[14].attributes);
+    assert.isOk(res.body.data[14].attributes['created-at']);
+    assert.strictEqual(res.body.data[14].attributes.name, sampleData.categories.category14.name);
+    assert.strictEqual(res.body.data[14].id, category14Uuid);
+    assert.strictEqual(res.body.data[14].type, 'categories');
+
+    // Category 17
+    assert.isOk(res.body.data[15].attributes);
+    assert.isOk(res.body.data[15].attributes['created-at']);
+    assert.strictEqual(res.body.data[15].attributes.name, sampleData.categories.category17.name);
+    assert.strictEqual(res.body.data[15].id, category17Uuid);
+    assert.strictEqual(res.body.data[15].type, 'categories');
+
+    // Category 9
+    assert.isOk(res.body.data[16].attributes);
+    assert.isOk(res.body.data[16].attributes['created-at']);
+    assert.strictEqual(res.body.data[16].attributes.name, sampleData.categories.category9.name);
+    assert.strictEqual(res.body.data[16].id, category9Uuid);
+    assert.strictEqual(res.body.data[16].type, 'categories');
+
+    // Category 13
+    assert.isOk(res.body.data[17].attributes);
+    assert.isOk(res.body.data[17].attributes['created-at']);
+    assert.strictEqual(res.body.data[17].attributes.name, sampleData.categories.category13.name);
+    assert.strictEqual(res.body.data[17].id, category13Uuid);
+    assert.strictEqual(res.body.data[17].type, 'categories');
+
+    // Category 6
+    assert.isOk(res.body.data[18].attributes);
+    assert.isOk(res.body.data[18].attributes['created-at']);
+    assert.strictEqual(res.body.data[18].attributes.name, sampleData.categories.category6.name);
+    assert.strictEqual(res.body.data[18].id, category6Uuid);
+    assert.strictEqual(res.body.data[18].type, 'categories');
+
+    // Category 25
+    assert.isOk(res.body.data[19].attributes);
+    assert.isOk(res.body.data[19].attributes['created-at']);
+    assert.strictEqual(res.body.data[19].attributes.name, sampleData.categories.category25.name);
+    assert.strictEqual(res.body.data[19].id, category25Uuid);
+    assert.strictEqual(res.body.data[19].type, 'categories');
+
+    // Category 1
+    assert.isOk(res.body.data[20].attributes);
+    assert.isOk(res.body.data[20].attributes['created-at']);
+    assert.strictEqual(res.body.data[20].attributes.name, sampleData.categories.category1.name);
+    assert.strictEqual(res.body.data[20].id, category1Uuid);
+    assert.strictEqual(res.body.data[20].type, 'categories');
+
+    // Category 23
+    assert.isOk(res.body.data[21].attributes);
+    assert.isOk(res.body.data[21].attributes['created-at']);
+    assert.strictEqual(res.body.data[21].attributes.name, sampleData.categories.category23.name);
+    assert.strictEqual(res.body.data[21].id, category23Uuid);
+    assert.strictEqual(res.body.data[21].type, 'categories');
+
+    // Category 5
+    assert.isOk(res.body.data[22].attributes);
+    assert.isOk(res.body.data[22].attributes['created-at']);
+    assert.strictEqual(res.body.data[22].attributes.name, sampleData.categories.category5.name);
+    assert.strictEqual(res.body.data[22].id, category5Uuid);
+    assert.strictEqual(res.body.data[22].type, 'categories');
+
+    // Category 12
+    assert.isOk(res.body.data[23].attributes);
+    assert.isOk(res.body.data[23].attributes['created-at']);
+    assert.strictEqual(res.body.data[23].attributes.name, sampleData.categories.category12.name);
+    assert.strictEqual(res.body.data[23].id, category12Uuid);
+    assert.strictEqual(res.body.data[23].type, 'categories');
+
+    // Category 10
+    assert.isOk(res.body.data[24].attributes);
+    assert.isOk(res.body.data[24].attributes['created-at']);
+    assert.strictEqual(res.body.data[24].attributes.name, sampleData.categories.category10.name);
+    assert.strictEqual(res.body.data[24].id, category10Uuid);
+    assert.strictEqual(res.body.data[24].type, 'categories');
+
+    assert.isOk(res.body.meta);
+    assert.strictEqual(res.body.meta.pages, 2);
+    assert.strictEqual(res.body.meta.total, 27);
+  });
 
   it('should return 200 and 2 categories as user 1 with no limit and page=2');
 
