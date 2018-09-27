5b87d2ecd82fe12985e4bd00f3767424ec53e7a9
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-09-27 08:09:54 -0400

    model and tests

--- migrations/20180927104854-create-vendors-table.js
+++ migrations/20180927104854-create-vendors-table.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.UUID,
       },
-      created_at {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE,
       },
