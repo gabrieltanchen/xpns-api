faf0e320fea94ae2ae89310967231819925a4f74
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-10-08 11:49:27 -0400

    GET /vendors/:uuid

--- app/routes/vendors/index.js
+++ app/routes/vendors/index.js
@@ -1,6 +1,7 @@
 const { body } = require('express-validator/check');
 const getFn = require('./get');
 const postFn = require('./post');
+const routeItem = require('./item/');
 
 module.exports = (router, app) => {
   const Auditor = app.get('Auditor');
@@ -21,6 +22,8 @@ module.exports = (router, app) => {
       Auditor.trackApiCall(),
       postFn(app),
     );
+
+  routeItem(router, app);
 
   return router;
 };
--- tests/integration/vendors/item/get.js
+++ tests/integration/vendors/item/get.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const uuidv4 = require('uuid/v4');
 
+const sampleData = require('../../../sample-data/');
 const TestHelper = require('../../../test-helper/');
 
 const assert = chai.assert;
@@ -10,11 +10,22 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Integration - GET /vendors/:uuid', function() {
+  let controllers;
+  let models;
   let server;
   const testHelper = new TestHelper();
 
+  let user1Token;
+  let user1Uuid;
+  let user2Token;
+  let user2Uuid;
+  let vendorUuid;
+
   before('get server', async function() {
     this.timeout(30000);
+    const app = await testHelper.getApp();
+    controllers = app.get('controllers');
+    models = app.get('models');
     server = await testHelper.getServer();
   });
 
@@ -22,11 +33,104 @@ describe('Integration - GET /vendors/:uuid', function() {
     await testHelper.cleanup();
   });
 
-  it('should return 501', async function() {
+  beforeEach('create user 1', async function() {
+    const apiCall = await models.Audit.ApiCall.create();
+    user1Uuid = await controllers.UserCtrl.signUp({
+      auditApiCallUuid: apiCall.get('uuid'),
+      email: sampleData.users.user1.email,
+      firstName: sampleData.users.user1.firstName,
+      lastName: sampleData.users.user1.lastName,
+      password: sampleData.users.user1.password,
+    });
+  });
+
+  beforeEach('create user 1 token', async function() {
+    user1Token = await controllers.UserCtrl.getToken(user1Uuid);
+  });
+
+  beforeEach('create user 2', async function() {
+    const apiCall = await models.Audit.ApiCall.create();
+    user2Uuid = await controllers.UserCtrl.signUp({
+      auditApiCallUuid: apiCall.get('uuid'),
+      email: sampleData.users.user2.email,
+      firstName: sampleData.users.user2.firstName,
+      lastName: sampleData.users.user2.lastName,
+      password: sampleData.users.user2.password,
+    });
+  });
+
+  beforeEach('create user 2 token', async function() {
+    user2Token = await controllers.UserCtrl.getToken(user2Uuid);
+  });
+
+  beforeEach('create vendor', async function() {
+    const apiCall = await models.Audit.ApiCall.create({
+      user_uuid: user1Uuid,
+    });
+    vendorUuid = await controllers.VendorCtrl.createVendor({
+      auditApiCallUuid: apiCall.get('uuid'),
+      name: sampleData.vendors.vendor1.name,
+    });
+  });
+
+  afterEach('truncate tables', async function() {
+    await testHelper.truncateTables();
+  });
+
+  it('should return 401 with no auth token', async function() {
     const res = await chai.request(server)
-      .get(`/vendors/${uuidv4()}`)
+      .get(`/vendors/${vendorUuid}`)
       .set('Content-Type', 'application/vnd.api+json');
-    expect(res).to.have.status(501);
-    assert.deepEqual(res.body, {});
+    expect(res).to.have.status(401);
+    assert.deepEqual(res.body, {
+      errors: [{
+        detail: 'Unauthorized',
+      }],
+    });
+  });
+
+  it('should return 404 with the wrong auth token', async function() {
+    const res = await chai.request(server)
+      .get(`/vendors/${vendorUuid}`)
+      .set('Content-Type', 'application/vnd.api+json')
+      .set('Authorization', `Bearer ${user2Token}`);
+    expect(res).to.have.status(404);
+    assert.deepEqual(res.body, {
+      errors: [{
+        detail: 'Not found',
+      }],
+    });
+  });
+
+  it('should return 404 when the vendor is soft deleted', async function() {
+    await models.Vendor.destroy({
+      where: {
+        uuid: vendorUuid,
+      },
+    });
+    const res = await chai.request(server)
+      .get(`/vendors/${vendorUuid}`)
+      .set('Content-Type', 'application/vnd.api+json')
+      .set('Authorization', `Bearer ${user1Token}`);
+    expect(res).to.have.status(404);
+    assert.deepEqual(res.body, {
+      errors: [{
+        detail: 'Not found',
+      }],
+    });
+  });
+
+  it('should return 200 with the correct auth token', async function() {
+    const res = await chai.request(server)
+      .get(`/vendors/${vendorUuid}`)
+      .set('Content-Type', 'application/vnd.api+json')
+      .set('Authorization', `Bearer ${user1Token}`);
+    expect(res).to.have.status(200);
+    assert.isOk(res.body.data);
+    assert.isOk(res.body.data.attributes);
+    assert.isOk(res.body.data.attributes['created-at']);
+    assert.strictEqual(res.body.data.attributes.name, sampleData.vendors.vendor1.name);
+    assert.strictEqual(res.body.data.id, vendorUuid);
+    assert.strictEqual(res.body.data.type, 'vendors');
   });
 });
