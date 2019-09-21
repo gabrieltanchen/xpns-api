0861b23540c02e7df7b239b0e602d5e398e04604
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-21 08:28:54 -0400

    Change package name back (#89)

--- config/index.js
+++ config/index.js
@@ -8,11 +8,11 @@ nconf.env();
 /* istanbul ignore next */
 switch (nconf.get('NODE_ENV')) {
 case 'development':
-  nconf.set('DATABASE_URL', 'postgres://expense:expense@localhost/expense_development');
+  nconf.set('DATABASE_URL', 'postgres://finance:finance@localhost/finance_development');
   nconf.set('jwtPrivateKey', 'wD9!$PKRwjd4TG6hB@MYmx5FgVLMRGmCs$p=cupWxb3%^R86hC@7W8j=97JkzYMK');
   break;
 case 'test':
-  nconf.set('DATABASE_URL', 'postgres://expense:expense@localhost/expense_test');
+  nconf.set('DATABASE_URL', 'postgres://finance:finance@localhost/finance_test');
   nconf.set('jwtPrivateKey', 'fu&-?4XFPQM^5H@NcZ-#_K6t5XaePQn4-F5?yahA^xX+ywUAzJG5qDpDL6U9PvLn');
   break;
 default:
