af9ce3c2b7156ccf0c26b52315a0b64baccfe562
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-01-01 17:33:09 -0500

    household member deletion

--- tests/unit/controllers/household-ctrl/delete-member.js
+++ tests/unit/controllers/household-ctrl/delete-member.js
@@ -228,4 +228,101 @@ describe('Unit:Controllers - HouseholdCtrl.deleteMember', function() {
     assert.isNotOk(trackChangesParams.newList);
     assert.isOk(trackChangesParams.transaction);
   });
+
+  describe('when the household member has an expense', function() {
+    let expenseUuid;
+    let subcategoryUuid;
+    let vendorUuid;
+
+    beforeEach('create subcategory', async function() {
+      const category = await models.Category.create({
+        household_uuid: user1HouseholdUuid,
+        name: sampleData.categories.category1.name,
+      });
+      const subcategory = await models.Subcategory.create({
+        category_uuid: category.get('uuid'),
+        name: sampleData.categories.category2.name,
+      });
+      subcategoryUuid = subcategory.get('uuid');
+    });
+
+    beforeEach('create vendor', async function() {
+      const vendor = await models.Vendor.create({
+        household_uuid: user1HouseholdUuid,
+        name: sampleData.vendors.vendor1.name,
+      });
+      vendorUuid = vendor.get('uuid');
+    });
+
+    beforeEach('create expense', async function() {
+      const expense = await models.Expense.create({
+        amount_cents: sampleData.expenses.expense1.amount_cents,
+        date: sampleData.expenses.expense1.date,
+        description: sampleData.expenses.expense1.description,
+        household_member_uuid: householdMemberUuid,
+        reimbursed_cents: sampleData.expenses.expense1.reimbursed_cents,
+        subcategory_uuid: subcategoryUuid,
+        vendor_uuid: vendorUuid,
+      });
+      expenseUuid = expense.get('uuid');
+    });
+
+    it('should reject when deleting the household member', async function() {
+      try {
+        const apiCall = await models.Audit.ApiCall.create({
+          user_uuid: user1Uuid,
+        });
+        await controllers.HouseholdCtrl.deleteMember({
+          auditApiCallUuid: apiCall.get('uuid'),
+          householdMemberUuid,
+        });
+        /* istanbul ignore next */
+        throw new Error('Expected to reject not resolve.');
+      } catch (err) {
+        assert.isOk(err);
+        assert.strictEqual(err.message, 'Cannot delete with expenses');
+        assert.isTrue(err instanceof HouseholdError);
+      }
+      assert.strictEqual(trackChangesSpy.callCount, 0);
+    });
+
+    it('should resolve if the expense is deleted', async function() {
+      await models.Expense.destroy({
+        where: {
+          uuid: expenseUuid,
+        },
+      });
+
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.HouseholdCtrl.deleteMember({
+        auditApiCallUuid: apiCall.get('uuid'),
+        householdMemberUuid,
+      });
+
+      // Verify that the HouseholdMember instance is deleted.
+      const householdMember = await models.HouseholdMember.findOne({
+        attributes: ['uuid'],
+        where: {
+          uuid: householdMemberUuid,
+        },
+      });
+      assert.isNull(householdMember);
+
+      assert.strictEqual(trackChangesSpy.callCount, 1);
+      const trackChangesParams = trackChangesSpy.getCall(0).args[0];
+      assert.strictEqual(trackChangesParams.auditApiCallUuid, apiCall.get('uuid'));
+      assert.isNotOk(trackChangesParams.changeList);
+      assert.isOk(trackChangesParams.deleteList);
+      const deleteCategory = _.find(trackChangesParams.deleteList, (deleteInstance) => {
+        return deleteInstance instanceof models.HouseholdMember
+          && deleteInstance.get('uuid') === householdMemberUuid;
+      });
+      assert.isOk(deleteCategory);
+      assert.strictEqual(trackChangesParams.deleteList.length, 1);
+      assert.isNotOk(trackChangesParams.newList);
+      assert.isOk(trackChangesParams.transaction);
+    });
+  });
 });
