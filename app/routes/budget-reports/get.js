5e63857a2a6ad73e449903d4374cc416e2057f3e
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-01-05 08:38:04 -0500

    [feature] Add budget report to dashboard (#145)
    * Budget reports route
    
    * integration tests

---
+++ app/routes/budget-reports/get.js
@@ -0,0 +1,149 @@
+const moment = require('moment');
+const Sequelize = require('sequelize');
+
+const Op = Sequelize.Op;
+
+module.exports = (app) => {
+  const models = app.get('models');
+
+  return async(req, res, next) => {
+    try {
+      let year = moment().year();
+      if (req.query && req.query.year && !isNaN(parseInt(req.query.year, 10))) {
+        year = parseInt(req.query.year, 10);
+      }
+      let month = moment().month();
+      if (req.query && req.query.month && !isNaN(parseInt(req.query.month, 10))) {
+        month = parseInt(req.query.month, 10);
+      }
+      if (year < 2000 || year > 2050) {
+        return new Error('Invalid year provided');
+      }
+      if (month < 0 || month > 11) {
+        return new Error('Invalid month provided');
+      }
+
+      const user = await models.User.findOne({
+        attributes: ['household_uuid', 'uuid'],
+        where: {
+          uuid: req.userUuid,
+        },
+      });
+
+      const fromDate = moment([year, month]);
+      const toDate = fromDate.clone().add(1, 'M');
+
+      const subcategories = await models.Subcategory.findAll({
+        attributes: [
+          'name',
+          'uuid',
+          [Sequelize.fn('sum', Sequelize.col('Expenses.amount_cents')), 'amount_cents'],
+          [Sequelize.fn('sum', Sequelize.col('Expenses.reimbursed_cents')), 'reimbursed_cents'],
+        ],
+        group: ['Subcategory.uuid', 'Category.uuid', 'Budgets.uuid'],
+        include: [{
+          attributes: ['budget_cents', 'uuid'],
+          model: models.Budget,
+          required: false,
+          where: {
+            month,
+            year,
+          },
+        }, {
+          attributes: ['name', 'uuid'],
+          model: models.Category,
+          required: true,
+          where: {
+            household_uuid: user.get('household_uuid'),
+          },
+        }, {
+          attributes: [],
+          model: models.Expense,
+          required: false,
+          where: {
+            date: {
+              [Op.lt]: toDate.format('YYYY-MM-DD'),
+              [Op.gte]: fromDate.format('YYYY-MM-DD'),
+            },
+          },
+        }],
+        order: [
+          ['Category', 'name', 'ASC'],
+          ['name', 'ASC'],
+        ],
+        where: {
+          [Op.or]: [
+            Sequelize.literal('"amount_cents" > 0'),
+            Sequelize.literal('"reimbursed_cents" > 0'),
+            Sequelize.literal('"Budgets"."budget_cents" > 0'),
+          ],
+        },
+      });
+
+      const included = [];
+      const categoryIds = [];
+      subcategories.forEach((subcategory) => {
+        included.push({
+          'attributes': {
+            'name': subcategory.get('name'),
+          },
+          'id': subcategory.get('uuid'),
+          'type': 'subcategories',
+        });
+        if (!categoryIds.includes(subcategory.Category.get('uuid'))) {
+          categoryIds.push(subcategory.Category.get('uuid'));
+          included.push({
+            'attributes': {
+              'name': subcategory.Category.get('name'),
+            },
+            'id': subcategory.Category.get('uuid'),
+            'type': 'categories',
+          });
+        }
+      });
+
+      return res.status(200).json({
+        'data': subcategories.map((subcategory) => {
+          const amountCents = subcategory.get('amount_cents') || 0;
+          const reimbursedCents = subcategory.get('reimbursed_cents') || 0;
+          const actualCents = amountCents - reimbursedCents;
+          const budgetCents = subcategory.Budgets
+            && subcategory.Budgets[0]
+            && subcategory.Budgets[0].get('budget_cents')
+            ? subcategory.Budgets[0].get('budget_cents')
+            : 0;
+          const differenceCents = budgetCents - actualCents;
+          return {
+            'attributes': {
+              'actual': parseFloat(actualCents / 100),
+              'actual-cents': actualCents,
+              'budget': parseFloat(budgetCents / 100),
+              'budget-cents': budgetCents,
+              'difference': parseFloat(differenceCents / 100),
+              'difference-cents': differenceCents,
+            },
+            'id': `${subcategory.get('uuid')}-${year}-${month}`,
+            'relationships': {
+              'category': {
+                'data': {
+                  'id': subcategory.Category.get('uuid'),
+                  'type': 'categories',
+                },
+              },
+              'subcategory': {
+                'data': {
+                  'id': subcategory.get('uuid'),
+                  'type': 'subcategories',
+                },
+              },
+            },
+            'type': 'budget-reports',
+          };
+        }),
+        'included': included,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  };
+};
