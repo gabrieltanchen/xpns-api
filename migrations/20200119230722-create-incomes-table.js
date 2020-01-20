86ac86105e12e88e879420b6efe5f149287593e3
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-01-19 21:21:37 -0500

    migration

---
+++ migrations/20200119230722-create-incomes-table.js
@@ -0,0 +1,45 @@
+'use strict';
+/* istanbul ignore next */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('incomes', {
+      uuid: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      household_member_uuid: {
+        allowNull: false,
+        references: {
+          key: 'uuid',
+          model: 'household_members',
+        },
+        type: Sequelize.UUID,
+      },
+      amount_cents: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('incomes');
+  },
+};
