0e7c1daec2769bedd242d55dc956c433fabf1511
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-01-25 09:05:01 -0500

    rest of IncomeCtrl.createIncome tests

--- tests/unit/controllers/income-ctrl/create-income.js
+++ tests/unit/controllers/income-ctrl/create-income.js
@@ -22,7 +22,6 @@ describe('Unit:Controllers - IncomeCtrl.createIncome', function() {
   let user1Uuid;
   let user2HouseholdMemberUuid;
   let user2HouseholdUuid;
-  let user2Uuid;
 
   before('get app', async function() {
     this.timeout(30000);
@@ -115,23 +114,236 @@ describe('Unit:Controllers - IncomeCtrl.createIncome', function() {
     assert.strictEqual(trackChangesSpy.callCount, 0);
   });
 
-  it('should reject with no amount');
+  it('should reject with no amount', async function() {
+    try {
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: null,
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Invalid amount');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject with an invalid amount');
+  it('should reject with an invalid amount', async function() {
+    try {
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: 'invalid amount',
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Invalid amount');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject with no description');
+  it('should reject with no description', async function() {
+    try {
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: null,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Invalid description');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject with an invalid description');
+  it('should reject with an invalid description', async function() {
+    try {
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: 1234,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Invalid description');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject with no audit API call');
+  it('should reject with no audit API call', async function() {
+    try {
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: null,
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Missing audit API call');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject when the audit API call does not exist');
+  it('should reject when the audit API call does not exist', async function() {
+    try {
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: uuidv4(),
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Missing audit API call');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject when the user does not exist');
+  it('should reject when the user does not exist', async function() {
+    try {
+      await models.User.destroy({
+        where: {
+          uuid: user1Uuid,
+        },
+      });
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: user1HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Audit user does not exist');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject when the household member does not exist');
+  it('should reject when the household member does not exist', async function() {
+    try {
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: uuidv4(),
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Household member not found');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should reject when the household member belongs to a different household');
+  it('should reject when the household member belongs to a different household', async function() {
+    try {
+      const apiCall = await models.Audit.ApiCall.create({
+        user_uuid: user1Uuid,
+      });
+      await controllers.IncomeCtrl.createIncome({
+        amountCents: sampleData.incomes.income1.amount_cents,
+        auditApiCallUuid: apiCall.get('uuid'),
+        description: sampleData.incomes.income1.description,
+        householdMemberUuid: user2HouseholdMemberUuid,
+      });
+      /* istanbul ignore next */
+      throw new Error('Expected to reject not resolve.');
+    } catch (err) {
+      assert.isOk(err);
+      assert.strictEqual(err.message, 'Household member not found');
+      assert.isTrue(err instanceof IncomeError);
+    }
+    assert.strictEqual(trackChangesSpy.callCount, 0);
+  });
 
-  it('should resolve creating an income');
+  it('should resolve creating an income', async function() {
+    const apiCall = await models.Audit.ApiCall.create({
+      user_uuid: user1Uuid,
+    });
+    const incomeUuid = await controllers.IncomeCtrl.createIncome({
+      amountCents: sampleData.incomes.income1.amount_cents,
+      auditApiCallUuid: apiCall.get('uuid'),
+      description: sampleData.incomes.income1.description,
+      householdMemberUuid: user1HouseholdMemberUuid,
+    });
+
+    assert.isOk(incomeUuid);
+
+    // Verify the Income instance.
+    const income = await models.Income.findOne({
+      attributes: [
+        'amount_cents',
+        'description',
+        'household_member_uuid',
+        'uuid',
+      ],
+      where: {
+        uuid: incomeUuid,
+      },
+    });
+    assert.isOk(income);
+    assert.strictEqual(income.get('amount_cents'), sampleData.incomes.income1.amount_cents);
+    assert.strictEqual(income.get('description'), sampleData.incomes.income1.description);
+    assert.strictEqual(income.get('household_member_uuid'), user1HouseholdMemberUuid);
+
+    assert.strictEqual(trackChangesSpy.callCount, 1);
+    const trackChangesParams = trackChangesSpy.getCall(0).args[0];
+    assert.strictEqual(trackChangesParams.auditApiCallUuid, apiCall.get('uuid'));
+    assert.isNotOk(trackChangesParams.changeList);
+    assert.isNotOk(trackChangesParams.deleteList);
+    assert.isOk(trackChangesParams.newList);
+    const newExpense = _.find(trackChangesParams.newList, (newInstance) => {
+      return newInstance instanceof models.Income
+        && newInstance.get('uuid') === income.get('uuid');
+    });
+    assert.isOk(newExpense);
+    assert.strictEqual(trackChangesParams.newList.length, 1);
+    assert.isOk(trackChangesParams.transaction);
+  });
 });
