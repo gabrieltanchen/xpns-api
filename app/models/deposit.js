a4d5c643f2957993c42b6c218fbbbc4044478393
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-06-28 20:52:06 -0400

    fix linter

--- app/models/deposit.js
+++ app/models/deposit.js
@@ -1,40 +1,40 @@
 const Sequelize = require('sequelize');
 
 module.exports = (sequelize) => {
-	return sequelize.define('Deposit', {
-		amount_cents: {
-			allowNull: false,
-			type: Sequelize.INTEGER,
-		},
-		created_at: {
-			allowNull: false,
-			type: Sequelize.DATE,
-		},
-		date: {
-			allowNull: false,
-			type: Sequelize.DATEONLY,
-		},
-		deleted_at: {
-			allowNull: true,
-			type: Sequelize.DATE,
-		},
-		fund_uuid: {
-			allowNull: false,
-			type: Sequelize.UUID,
-		},
-		updated_at: {
-			allowNull: false,
-			type: Sequelize.DATE,
-		},
-		uuid: {
-			allowNull: false,
-			defaultValue: Sequelize.UUIDV4,
-			primaryKey: true,
-			type: Sequelize.UUID,
-		},
-	}, {
-		paranoid: true,
-		tableName: 'deposits',
-		timestamps: true,
-	});
+  return sequelize.define('Deposit', {
+    amount_cents: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    },
+    created_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    date: {
+      allowNull: false,
+      type: Sequelize.DATEONLY,
+    },
+    deleted_at: {
+      allowNull: true,
+      type: Sequelize.DATE,
+    },
+    fund_uuid: {
+      allowNull: false,
+      type: Sequelize.UUID,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    uuid: {
+      allowNull: false,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      type: Sequelize.UUID,
+    },
+  }, {
+    paranoid: true,
+    tableName: 'deposits',
+    timestamps: true,
+  });
 };
