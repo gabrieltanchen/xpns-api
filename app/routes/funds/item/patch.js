1206eef5ce84539094015cb67e28b83c304cd560
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-07-03 23:11:28 -0400

    PATCH /funds/:uuid

---
+++ app/routes/funds/item/patch.js
@@ -0,0 +1,56 @@
+module.exports = (app) => {
+  const controllers = app.get('controllers');
+  const models = app.get('models');
+
+  /**
+   * @api {patch} /funds/:uuid
+   * @apiName FundItemPatch
+   * @apiGroup Fund
+   *
+   * @apiSuccess (200) {object} data
+   * @apiSuccess (200) {object} data.attributes
+   * @apiSuccess (200) {integer} data.attributes.amount
+   * @apiSuccess (200) {string} data.attributes[created-at]
+   * @apiSuccess (200) {string} data.attributes.name
+   * @apiSuccess (200) {string} data.id
+   * @apiSuccess (200) {string} data.type
+   *
+   * @apiErrorExample {json} Error-Response:
+   *    HTTP/1.1 401 Unprocessable Entity
+   *    {
+   *      "errors": [{
+   *        "detail": "Unauthorized",
+   *      }],
+   *    }
+   */
+  return async(req, res, next) => {
+    try {
+      await controllers.FundCtrl.updateFund({
+        auditApiCallUuid: req.auditApiCallUuid,
+        fundUuid: req.params.uuid,
+        name: req.body.data.attributes.name,
+      });
+
+      const fund = await models.Fund.findOne({
+        attributes: ['amount_cents', 'created_at', 'name', 'uuid'],
+        where: {
+          uuid: req.params.uuid,
+        },
+      });
+
+      return res.status(200).json({
+        'data': {
+          'attributes': {
+            'amount': fund.get('amount_cents'),
+            'created-at': fund.get('created_at'),
+            'name': fund.get('name'),
+          },
+          'id': fund.get('uuid'),
+          'type': 'funds',
+        },
+      });
+    } catch (err) {
+      return next(err);
+    }
+  };
+};
